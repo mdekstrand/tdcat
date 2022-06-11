01d723a1ba9ca14a5a4fda7f4d549c74584b7f0c
Michael Ekstrand <md@ekstrandom.net>
2022-06-11 16:38:17 -0600

    parse output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use structopt::StructOpt;
+use anyhow::Result;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name="tdcat")]
+struct CLIOptions {
+  /// Increase verbosity of logging output.
+  #[structopt(short="v", long="verbose", parse(from_occurrences))]
+  verbosity: usize,
+  /// Silence non-error logging
+  #[structopt(short="q", long="quiet")]
+  quiet: bool,
+}
+
+fn main() -> Result<()> {
+  let opts = CLIOptions::from_args();
+  stderrlog::new().quiet(opts.quiet).verbosity(opts.verbosity).init()?;
+  Ok(())
 }
